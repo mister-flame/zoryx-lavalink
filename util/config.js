6095fd558a2b7b3399de9d01c5416593e4716358
MR.Flame <j0k3r779209@gmail.com>
2026-09-05 10:22:57 +0200

    Change of the base color for embeds

--- util/config.js
+++ util/config.js
@@ -9,7 +9,7 @@ try {
 exports.token = config ? config.token : process.env.token;
 exports.node = config ? config.node : process.env.node;
 exports.START_WEBHOOK = config ? config.START_WEBHOOK : process.env.START_WEBHOOK;
-exports.COLOR_EMBED = config ? config.COLOR_EMBED || "#0099ff" : process.env.COLOR_EMBED || "#0099ff";
+exports.COLOR_EMBED = config ? config.COLOR_EMBED || "#FF4E3A" : process.env.COLOR_EMBED || "#FF4E3A";
 exports.dbPath = config ? config.dbPath || "/home/mr-flame/Desktop/Lavalink-BOT-Test/databases/tcDB.db" : process.env.dbPath || "/home/mr-flame/Desktop/Lavalink-BOT-Test/databases/tcDB.db";
 exports.LOGS_WEBHOOK = config ? config.LOGS_WEBHOOK : process.env.LOGS_WEBHOOK;
 exports.clientId = config ? config.clientId || "1424383941502173306" : process.env.clientId || "1424383941502173306";
